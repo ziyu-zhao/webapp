1cc4d6c16a73bf9ea451e84765e9a4a8a1a0eff7
ziyu-zhao <zhao.ziyu2@northeastern.edu>
2021-11-01 21:01:41 +0800

    update

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #java -jar /home/ubuntu/web/web.jar >/dev/null 2>&1 &
-java -jar /usr/local/web.jar > /dev/null 2>&1 &
+java -jar /usr/local/web/web.jar > /dev/null 2>&1 &
--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo pkill -9 java
-sudo rm -rf /usr/local/web.jar
+sudo rm -rf /usr/local/web/web.jar
