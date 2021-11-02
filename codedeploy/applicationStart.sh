f42560c3026fe87d9cf1b57bbef5fe8b87e24067
ziyu-zhao <zhao.ziyu2@northeastern.edu>
2021-11-02 08:24:00 +0800

    update

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 #java -jar /home/ubuntu/web/web.jar >/dev/null 2>&1 &
-java -jar /usr/local/web/web.jar > /usr/local/web/log
+#java -jar /usr/local/web/web.jar > /usr/local/web/log
+
+/usr/local/web/start.sh
