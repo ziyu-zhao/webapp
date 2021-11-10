239e5b996a2cc8e8a06e9caad35a1094ce0833c0
ziyu-zhao <zhao.ziyu2@northeastern.edu>
2021-11-10 14:09:01 +0800

    test prod

--- src/main/java/com/example/csye6225_zzy/log/DiyLog.java
+++ src/main/java/com/example/csye6225_zzy/log/DiyLog.java
@@ -28,7 +28,7 @@ public class DiyLog {
         Object result = joinPoint.proceed();
         long executeTime2 = System.currentTimeMillis();
         statsDClient.recordExecutionTime(sign,(executeTime2-executeTime1));
-        return result + EC2MetadataUtils.getPrivateIpAddress();
+        return result + " \nlocal address: "+EC2MetadataUtils.getPrivateIpAddress();
     }
 
     @Around("execution(* com.example.csye6225_zzy.service.*.*(..))")
