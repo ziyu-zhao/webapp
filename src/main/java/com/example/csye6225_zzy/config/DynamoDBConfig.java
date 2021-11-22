27237ebfbe15047a0ba0ede60fa7099caaa37ec8
ziyu-zhao <zhao.ziyu2@northeastern.edu>
2021-11-22 18:24:20 +0800

    test prod

--- src/main/java/com/example/csye6225_zzy/config/DynamoDBConfig.java
+++ src/main/java/com/example/csye6225_zzy/config/DynamoDBConfig.java
@@ -1,5 +1,8 @@
 package com.example.csye6225_zzy.config;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
@@ -11,7 +14,12 @@ public class DynamoDBConfig {
 
     @Bean
     public DynamoDB dynamoDB(){
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        AWSCredentialsProvider awsCredentialsProvider = new InstanceProfileCredentialsProvider(false);
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(awsCredentialsProvider)
+                .withRegion(Regions.US_EAST_1)
+                .build();
         return new DynamoDB(client);
     }
 }
