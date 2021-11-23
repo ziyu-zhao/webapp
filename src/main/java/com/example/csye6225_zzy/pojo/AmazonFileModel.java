c00c7cc1fe9632acde2eccba3f07d35374f373d8
ziyu-zhao <zhao.ziyu2@northeastern.edu>
2021-11-23 14:53:53 +0800

    test prod

--- src/main/java/com/example/csye6225_zzy/pojo/AmazonFileModel.java
+++ src/main/java/com/example/csye6225_zzy/pojo/AmazonFileModel.java
@@ -7,13 +7,14 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "amazon_file_model")
-public class AmazonFileModel {
+public class AmazonFileModel implements Serializable {
 
     private String filename;
 
